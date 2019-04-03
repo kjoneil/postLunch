6f9cbf025bdac2ee3fcc7678b01bd9987d04ad99
kjoneil <kjoneil.rw@gmail.com>
2019-04-03 13:08:03 -0500

    blah

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/index.html", function(req, res) {
+app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
 
