fabbe4cdee99114df3e54c799b73905bc97a9892
kjoneil <kjoneil.rw@gmail.com>
2019-04-03 13:02:24 -0500

    blah

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function(req, res) {
+app.get("/index.html", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
 
